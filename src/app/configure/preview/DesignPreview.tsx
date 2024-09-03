28b088d6b2f103ae426002cdf353f5916e227c84
Atharva Kharage <atharvak1520@gmail.com>
2024-09-02 17:15:44 -0700

    ...

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -156,7 +156,7 @@ const DesignPreview = ({configuration}: {configuration: Configuration}) => {
                             </div>
                         </div>
                         <div className='mt-8 flex justify-end pb-12'>
-                            <Button isLoading={isPending} disabled={isPending} loadingText="Just a moment..." onClick={() => handleCheckout()} className='px-4 sm:px-6 lg:px-8'>Check out <ArrowRight className='h-4 w-4 ml-1.5 inline'/></Button>
+                            <Button isLoading={isPending} disabled={isPending} loadingText="Just a moment..." onClick={() => createPaymentSession({ configId: id })} className='px-4 sm:px-6 lg:px-8'>Check out <ArrowRight className='h-4 w-4 ml-1.5 inline'/></Button>
                         </div>
                     </div>
                 </div>
